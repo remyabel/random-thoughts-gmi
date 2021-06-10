f5f1c220cc5307b6f159cde1720be939b9781b82
Tommy Nguyen <remyabel@gmail.com>
2021-06-10 01:37:03 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rm -rf random-thoughts-gmi
-git clone --depth=1 https://tildegit.org/remyabel/random-thoughts-gmi
-rm public_gemini/*.gmi
-cp -a random-thoughts-gmi/*.gmi public_gemini/
-cd public_gemini || exit
+rm "$HOME"/public_gemini/*.gmi
+cp -a *.gmi "$HOME"/public_gemini/
+cd "$HOME"/public_gemini || exit
 for file in *.gmi; do
   if [[ $file =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.* ]]; then
     date=$(echo "$file" | grep -Eo '^[0-9]{4}-[0-9]{2}-[0-9]{2}')
