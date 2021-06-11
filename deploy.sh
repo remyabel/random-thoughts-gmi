3d2a7af460984d85fb03c3946897f171db0ecc41
Tommy Nguyen <remyabel@gmail.com>
2021-06-10 23:31:07 -0400

    Fix arithmetic expression

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ for file in *.gmi; do
   if [[ $file =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.* ]]; then
     date=$(echo "$file" | grep -Eo '^[0-9]{4}-[0-9]{2}-[0-9]{2}')
     touch -d "$date 00:00:0$idx" "$file"
-    idx=$idx+1
+    idx=$(($idx+1))
   fi
 done
 "$HOME"/.local/bin/gemfeed -a remyabel -b gemini://tilde.team/~remyabel/ -d "$HOME"/public_gemini -e remyabel@tilde.team
