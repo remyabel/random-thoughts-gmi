eb42f404980cc0b3b418f1f88dfe7313d0ab41ce
Tommy Nguyen <remyabel@gmail.com>
2021-06-10 23:28:42 -0400

    Increase timestamp to make articles on the same day newer

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 rm "$HOME"/public_gemini/*.gmi
 cp -a *.gmi "$HOME"/public_gemini/
 cd "$HOME"/public_gemini || exit
+idx=0
 for file in *.gmi; do
   if [[ $file =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.* ]]; then
     date=$(echo "$file" | grep -Eo '^[0-9]{4}-[0-9]{2}-[0-9]{2}')
-    touch -d "$date 00:00:00" "$file"
+    touch -d "$date 00:00:0$idx" "$file"
+    idx=$idx+1
   fi
 done
 "$HOME"/.local/bin/gemfeed -a remyabel -b gemini://tilde.team/~remyabel/ -d "$HOME"/public_gemini -e remyabel@tilde.team
